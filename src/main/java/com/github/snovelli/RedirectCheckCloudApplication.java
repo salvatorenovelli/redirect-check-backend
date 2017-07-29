c04799bd3d8ff209326cc0335d1be04af8705aef
salvatorenovelli <salvatore.novelli@gmail.com>
2017-07-29 15:18:15 +0100

    INIT

---
+++ src/main/java/com/github/snovelli/RedirectCheckCloudApplication.java
@@ -0,0 +1,12 @@
+package com.github.snovelli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedirectCheckCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedirectCheckCloudApplication.class, args);
+	}
+}
