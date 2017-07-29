2aa04e07a9c19eb33e676d806b2773f3d4d2fe95
salvatorenovelli <salvatore.novelli@gmail.com>
2017-07-29 15:22:33 +0100

    Adding test controller

--- src/main/java/com/github/snovelli/RedirectCheckCloudApplication.java
+++ src/main/java/com/github/snovelli/RedirectCheckCloudApplication.java
@@ -2,11 +2,23 @@ package com.github.snovelli;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class RedirectCheckCloudApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RedirectCheckCloudApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RedirectCheckCloudApplication.class, args);
+    }
+
+
+    @RestController
+    class MyController {
+
+        @GetMapping("/hello")
+        public String sayHello() {
+            return "Hello!";
+        }
+    }
 }
