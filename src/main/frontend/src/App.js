59a837a7c394461886ef5cdedfe85b149ad60e10
salvatorenovelli <salvatore.novelli@gmail.com>
2017-08-07 20:23:11 +0100

    Added reverse-proxy for development

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,21 +1,108 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import Dropzone from 'react-dropzone'
+import Request from 'superagent'
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <h2>Welcome to Redirect Check!</h2>
+                </div>
+                <Basic/>
+
+                <Tasks />
+
+
+
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
+            </div>
+        );
+    }
 }
+
+
+class Tasks extends Component{
+    render(){
+        return(
+            <table class="table">
+                <thead>
+                <tr>
+                    <th>Source File</th>
+                    <th>Status</th>
+                    <th>Output</th>
+                </tr>
+                </thead>
+                <tbody>
+
+                {/*<tr th:each="task : ${tasks}">*/}
+                    {/*<td th:text="${task.getInputFileName()}"></td>*/}
+                    {/*<td th:text="${task.getStatus() + ' '+ task.getTaskProgress().getPerventageCompleted()} + '%'"></td>*/}
+                    {/*<td><a th:href="${'/files/' + task.getOutputUri()}" th:text="${task.getOutputUri()}"/></td>*/}
+                {/*</tr>*/}
+                </tbody>
+            </table>
+        );
+    }
+}
+
+class Basic extends Component {
+    constructor() {
+        super();
+        this.state = {files: []}
+    }
+
+    onDrop(files) {
+        this.setState({
+            files
+        });
+
+        const req = Request.post('api/upload');
+        files.forEach(file => {
+            req.attach("file", file);
+        });
+
+        req.end(() => console.log("done"));
+    }
+
+    render() {
+        return (
+            <section>
+                <div className="dropzone">
+                    <Dropzone onDrop={this.onDrop.bind(this)}>
+                        <p>Try dropping some files here, or click to select files to upload.</p>
+                    </Dropzone>
+                </div>
+                <aside>
+                    <h2>Dropped files</h2>
+                    <ul>
+                        {
+                            this.state.files.map(f => <li>{f.name} - {f.size} bytes</li>)
+                        }
+                    </ul>
+                </aside>
+            </section>
+        );
+    }
+}
+
+//
+// class UploadForm extends Component {
+//     render() {
+//         return (
+//             <form className="form-group" method="POST" encType="multipart/form-data" action="/">
+//                 <div className="form-group">
+//                     <label>File to upload
+//                         <input className="form-control-file" type="file" name="file" id="file"/>
+//                     </label>
+//                 </div>
+//                 <button className="btn btn-primary" type="submit">Upload</button>
+//             </form>
+//         );
+//     }
+// }
 
 export default App;
--- src/main/java/com/github/snovelli/TaskQueue.java
+++ src/main/java/com/github/snovelli/TaskQueue.java
@@ -2,6 +2,8 @@ package com.github.snovelli;
 
 import com.github.snovelli.model.RedirectCheckTask;
 import com.github.snovelli.model.RedirectCheckTaskRunner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.SessionScope;
@@ -12,18 +14,24 @@ import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @SessionScope
 @Component
 public class TaskQueue {
 
 
+    private static final Logger logger = LoggerFactory.getLogger(TaskQueue.class);
+    private static final AtomicInteger instance = new AtomicInteger(0);
     private final List<RedirectCheckTask> tasksPerUser = new CopyOnWriteArrayList<>();
     private final ExecutorService executorService;
+    private final int instanceNumber;
 
 
     @Autowired
     public TaskQueue(ExecutorService executorService) {
+        instanceNumber = instance.getAndIncrement();
+        logger.info("Instance {} created!", instanceNumber);
         this.executorService = executorService;
 
     }
@@ -37,6 +45,7 @@ public class TaskQueue {
 
 
     public List<RedirectCheckTask> listTasks() {
+        logger.info("Getting tasks for instance {}", instanceNumber);
         return tasksPerUser;
     }
 
