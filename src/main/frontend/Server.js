4de6245ddce7b521ede2902b4710070a90b8aeb5
salvatorenovelli <salvatore.novelli@gmail.com>
2017-08-12 11:02:48 +0100

    Minor refactoring in proxy configuration

--- src/main/frontend/Server.js
+++ src/main/frontend/Server.js
@@ -1,26 +1,33 @@
-var express  = require('express');
-var app      = express();
+let app = require('express')();
+// let httpServer = require("http").createServer(app);
+// let ws = require('ws');
+var expressWs = require('express-ws')(app);
 
-var httpProxy = require('http-proxy');
-var apiProxy = httpProxy.createProxyServer();
-var frontend = 'http://localhost:3000',
+let httpProxy = require('http-proxy');
+
+let apiProxy = httpProxy.createProxyServer();
+let frontend = 'http://localhost:3000',
     backend = 'http://localhost:8080';
 
 
 console.log("Proxy started!");
 
 
-
-app.all("/api/*", function(req, res) {
+app.all("/api/*", function (req, res) {
     console.log('redirecting to backend');
     apiProxy.web(req, res, {target: backend});
 });
 
-app.all("*",function(req, res) {
+app.all("*", function (req, res) {
     console.log('redirecting to frontend');
     apiProxy.web(req, res, {target: frontend});
 });
 
-
+app.ws('/', function(ws, req) {
+    ws.on('*', function(msg) {
+        console.log(msg);
+    });
+    console.log('socket', req.testing);
+});
 
 app.listen(3001);
