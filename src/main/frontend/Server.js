56b2d20a800f34c012ec9835f0dfc8c7c92371fa
salvatorenovelli <salvatore.novelli@gmail.com>
2017-08-20 19:35:48 +0100

    Simplifying dev script and adding uncaught exception handler

--- src/main/frontend/Server.js
+++ src/main/frontend/Server.js
@@ -9,14 +9,17 @@ let frontend = 'http://localhost:3000',
 
 console.log("Proxy started!");
 
+process.on('uncaughtException', function (err) {
+    console.log("Uncaught exception: " + err.message);
+});
 
 app.all("/api/*", function (req, res) {
-    console.log('redirecting to backend');
+    console.log('redirecting ' + req.originalUrl + ' to backend');
     apiProxy.web(req, res, {target: backend});
 });
 
 app.all("*", function (req, res) {
-    console.log('redirecting to frontend');
+    console.log('redirecting ' + req.originalUrl + ' to frontend');
     apiProxy.web(req, res, {target: frontend});
 });
 
