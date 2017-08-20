4d7348e1924f70b00f068d533ef5a7912a0c6134
salvatorenovelli <salvatore.novelli@gmail.com>
2017-08-20 20:48:59 +0100

    Added progress bar

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -51,27 +51,38 @@ class Tasks extends Component {
 
     render() {
         return (
-            <div className="jumbotron">
-                <table className="table text-left">
-                    <thead>
-                    <tr>
-                        <th>Source File</th>
-                        <th>Status</th>
-                        <th>Output</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {
-                        this.state.data.map((row, i) =>
-                            <tr key={i}>
-                                <td>{row.inputFileName}</td>
-                                <td>{row.status + ': ' + row.taskProgress.percentageCompleted + '%'}</td>
-                                <td><a href={'/api/files/' + row.outputUri}>{row.outputUri}</a></td>
-                            </tr>
-                        )
-                    }
-                    </tbody>
-                </table>
+            <div className="highlight">
+                <div className="table-responsive">
+                    <table className="table text-left col-md-12">
+                        <thead>
+                        <tr>
+                            <th className="col-sm-4 col-md-4 col-lg-4">Source File</th>
+                            <th className="col-sm-4 col-md-4 col-lg-4">Status</th>
+                            <th className="col-sm-4 col-md-4 col-lg-4">Output</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {
+                            this.state.data.map((row, i) =>
+                                <tr key={i}>
+                                    <td>{row.inputFileName}</td>
+                                    <td>
+                                        <div className="progress text-center">
+                                            <div className="progress-bar progress-bar-info progress-bar-striped"
+                                                 role="progressbar" aria-valuenow="60"
+                                                 aria-valuemin="0" aria-valuemax="100"
+                                                 style={{width: row.taskProgress.percentageCompleted + '%'}}>
+                                                {row.status}
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td><a href={'/api/files/' + row.outputUri}>{row.outputUri}</a></td>
+                                </tr>
+                            )
+                        }
+                        </tbody>
+                    </table>
+                </div>
             </div>
         );
     }
