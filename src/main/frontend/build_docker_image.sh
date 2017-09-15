311d7682f741f6b35d621c6f9a5cb7b303501075
Salvatore Novelli <salvatore.novelli@gmail.com>
2017-09-15 22:31:44 +0100

    Adding versioning to dockerfile

--- src/main/frontend/build_docker_image.sh
+++ src/main/frontend/build_docker_image.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
+
+export VERSION=`jq -r '.version' package.json`
+export APP_TAG=github.com/salvatorenovelli/redirect-check-frontend:${VERSION}
+
 yarn build
-docker build -t redirect-check-frontend:v1 .
+docker build -t ${APP_TAG} .
 
 
 #To run it locally you might want to use this :)
-# $ docker run -it --rm -p 5000:5000 redirect-check-frontend:v1
+# $ docker run -it --rm -p 5000:80 ${APP_TAG}
