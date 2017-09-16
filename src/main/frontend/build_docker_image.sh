2bb6c4f6ec667258eeac4312e19bdc2270cfd583
Salvatore Novelli <salvatore.novelli@gmail.com>
2017-09-16 23:03:26 +0100

    Testing build

--- src/main/frontend/build_docker_image.sh
+++ src/main/frontend/build_docker_image.sh
@@ -2,11 +2,22 @@
 
 
 export VERSION=`jq -r '.version' package.json`
-export APP_TAG=github.com/salvatorenovelli/redirect-check-frontend:${VERSION}
+export imageTag=github.com/salvatorenovelli/redirect-check-frontend:${VERSION}
 
 yarn build
-docker build -t ${APP_TAG} .
+
+echo "Building ${imageTag}"
+docker build -t ${imageTag} .
 
 
 #To run it locally you might want to use this :)
-# $ docker run -it --rm -p 5000:80 ${APP_TAG}
+# $ docker run -it --rm -p 5000:80 ${imageTag}
+
+
+sed -i.bak "s#<IMAGE_TAG_DO_NOT_EDIT>#${imageTag}#" k8s/production.yaml
+
+kubectl apply -f k8s/
+
+mv k8s/production.yaml.bak k8s/production.yaml
+
+
